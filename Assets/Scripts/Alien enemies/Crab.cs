6f2fdb6c49ba045cf70144439ea30ebf7041b083
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-03-28 22:02:09 +0100

    28/03/2024 22:02
    Arreglada inconsistencia con el collider y el rigidbody durante la muerte del enemigo Crab.

--- Assets/Scripts/Alien enemies/Crab.cs
+++ Assets/Scripts/Alien enemies/Crab.cs
@@ -29,10 +29,12 @@ public class Crab : MonoBehaviour
     private void Die()
     {
         // Al morir, al mismo tiempo se hacen invisible el objeto, se instancia la animación de muerte
-        // y se reproduce el sonido de muerte
+        // y se reproduce el sonido de muerte; se eliminan el rigidbody y el collider
         Color colorSprite = spRd.material.color;
         colorSprite.a = 0f;
         spRd.material.color = colorSprite;
+        Destroy(GetComponent<Collider2D>());
+        Destroy(GetComponent<Rigidbody2D>());
         Instantiate(deathEffect, transform.position, Quaternion.identity);
         sonidoMuerte.Play();
 
