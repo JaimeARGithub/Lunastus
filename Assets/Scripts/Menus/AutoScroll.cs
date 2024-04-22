5e6c6936159a0b44a98944083209d545950f7ad9
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-04-22 21:18:47 +0200

    22/04/2024 21:18
    Ajuste general de los canvas al tamaño de la pantalla y ajuste de la resolución de la pantalla. Recreación de escenas con ajuste de tamaño. Creación de escenas de instrucciones y créditos. Los créditos pasan, pendiente hacer que solo se vean en un área. Menú aún sin funcionalidad.

---
+++ Assets/Scripts/Menus/AutoScroll.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AutoScroll : MonoBehaviour
+{
+    private float speed = 25f;
+    private float textPositionBegin = -1800f;
+    private float textPositionEnd = 2040f;
+    private float textPositionEndResetLimit = 2035f;
+
+    RectTransform gameObjectRectTransform;
+    [SerializeField] public TextMeshProUGUI creditsText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObjectRectTransform = gameObject.GetComponent<RectTransform>();
+        StartCoroutine(AutoScrollText());
+    }
+
+    
+    private IEnumerator AutoScrollText()
+    {
+        while (gameObjectRectTransform.localPosition.y < textPositionEnd)
+        {
+            gameObjectRectTransform.Translate(Vector3.up * speed * Time.deltaTime);
+
+            if (gameObjectRectTransform.localPosition.y > textPositionEndResetLimit)
+            {
+                gameObjectRectTransform.localPosition = Vector3.up * textPositionBegin;
+            }
+
+            yield return null; 
+        }
+    }
+}
