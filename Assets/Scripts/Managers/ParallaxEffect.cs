a6d9209f3d7c8797eef29e059f189d446fe8e09f
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-05-05 00:35:41 +0200

    05/05/2024 00:36
    Parallax vertical implementado perfecto.

--- Assets/Scripts/Managers/ParallaxEffect.cs
+++ Assets/Scripts/Managers/ParallaxEffect.cs
@@ -8,6 +8,7 @@ public class ParallaxEffect : MonoBehaviour
     private Vector3 previousCameraPosition;
     private float parallaxMultiplier = 0.5f;
     private float spriteWidth, startPositionX;
+    private float spriteHeight, startPositionY;
 
 
     // Start is called before the first frame update
@@ -16,17 +17,23 @@ public class ParallaxEffect : MonoBehaviour
         cameraTransform = Camera.main.transform;
         previousCameraPosition = cameraTransform.position;
 
+
         spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
         startPositionX = transform.position.x;
+
+        spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+        startPositionY = transform.position.y;
     }
 
 
     void LateUpdate()
     {
         float deltaX = (cameraTransform.position.x - previousCameraPosition.x) * parallaxMultiplier;
+        float deltaY = (cameraTransform.position.y - previousCameraPosition.y) * parallaxMultiplier;
         float moveAmountX = cameraTransform.position.x * (1 - parallaxMultiplier);
+        float moveAmountY = cameraTransform.position.y * (1 - parallaxMultiplier);
 
-        transform.Translate(new Vector3(deltaX, 0, 0));
+        transform.Translate(new Vector3(deltaX, deltaY, 0));
         previousCameraPosition = cameraTransform.position;
 
 
@@ -39,6 +46,17 @@ public class ParallaxEffect : MonoBehaviour
         {
             transform.Translate(new Vector3(-spriteWidth,0,0));
             startPositionX -= spriteWidth;
+        }
+
+        if (moveAmountY > startPositionY + spriteHeight - 3)
+        {
+            transform.Translate(new Vector3(0, spriteHeight, 0));
+            startPositionY += spriteHeight;
+        }
+        else if (moveAmountY < startPositionY - spriteHeight + 3)
+        {
+            transform.Translate(new Vector3(0, -spriteHeight, 0));
+            startPositionY -= spriteHeight;
         }
     }
 }
