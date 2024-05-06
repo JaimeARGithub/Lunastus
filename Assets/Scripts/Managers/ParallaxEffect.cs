03c446b4516991c49db71dc748afd12534192be8
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-05-06 23:57:15 +0200

    06/05/2024 23:58
    funciona el parallax buenas noches finnius

--- Assets/Scripts/Managers/ParallaxEffect.cs
+++ Assets/Scripts/Managers/ParallaxEffect.cs
@@ -4,59 +4,75 @@ using UnityEngine;
 
 public class ParallaxEffect : MonoBehaviour
 {
-    private Transform cameraTransform;
+    // Multiplicador de velocidad
+    public float parallaxMultiplier = 0.5f;
+
+    // Posición de la cámara en el sprite anterior
     private Vector3 previousCameraPosition;
-    private float parallaxMultiplier = 0.5f;
-    private float spriteWidth, startPositionX;
-    private float spriteHeight, startPositionY;
 
+    // Dimensiones del sprite
+    private Vector2 spriteSize;
+    private float spriteWidth;
+    private float spriteHeight;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        cameraTransform = Camera.main.transform;
-        previousCameraPosition = cameraTransform.position;
-
-
-        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-        startPositionX = transform.position.x;
-
-        spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y;
-        startPositionY = transform.position.y;
+        // En el start ubicamos las dimensiones del sprite y a la posición anterior de la cámara
+        // le asignamos la actual
+        previousCameraPosition = Camera.main.transform.position;
+        spriteSize = GetComponent<SpriteRenderer>().bounds.size;
+        spriteWidth = spriteSize.x;
+        spriteHeight = spriteSize.y;
     }
 
-
-    void LateUpdate()
+    private void LateUpdate()
     {
-        float deltaX = (cameraTransform.position.x - previousCameraPosition.x) * parallaxMultiplier;
-        float deltaY = (cameraTransform.position.y - previousCameraPosition.y) * parallaxMultiplier;
-        float moveAmountX = cameraTransform.position.x * (1 - parallaxMultiplier);
-        float moveAmountY = cameraTransform.position.y * (1 - parallaxMultiplier);
+        // TRAS CADA FRAME, calculamos mediante un Vector3 la distancia entre la
+        // posición actual de la cámara y la posición anterior de la cámara
 
-        transform.Translate(new Vector3(deltaX, deltaY, 0));
-        previousCameraPosition = cameraTransform.position;
+        // Al sprite del parallax le aplicamos una variación del transform
+        // en los ejes X e Y igual a la distancia entre las posiciones actual
+        // y anterior de la cámara por un coeficiente de paralaje
+
+        // A la posición anterior de la cámara le asignamos la posición actual
+
+        Vector3 delta = Camera.main.transform.position - previousCameraPosition;
+        transform.position += new Vector3(delta.x * parallaxMultiplier, delta.y * parallaxMultiplier, 0f);
+        previousCameraPosition = Camera.main.transform.position;
 
 
 
-        if (moveAmountX > startPositionX + spriteWidth -3)
+        // Para dar sensación de continuidad al sprite del paralaje, se evalúa que :
+        // --El valor absoluto de la resta entre la X de la posición del sprite y la X de la posición de la cámara supere a la mitad del ancho
+        // --El valor absoluto de la resta entre la Y de la posición del sprite y la Y de la posición de la cámara supere a la mitad del alto
+
+        if (Mathf.Abs(transform.position.x - Camera.main.transform.position.x) > spriteWidth / 2 ||
+            Mathf.Abs(transform.position.y - Camera.main.transform.position.y) > spriteHeight / 2)
         {
-            transform.Translate(new Vector3(spriteWidth,0,0));
-            startPositionX += spriteWidth;
-        } else if (moveAmountX < startPositionX - spriteWidth +3)
-        {
-            transform.Translate(new Vector3(-spriteWidth,0,0));
-            startPositionX -= spriteWidth;
-        }
+            // Reposition sprite to the opposite side
+            Vector3 newSpritePosition = transform.position;
 
-        if (moveAmountY > startPositionY + spriteHeight - 3)
-        {
-            transform.Translate(new Vector3(0, spriteHeight, 0));
-            startPositionY += spriteHeight;
-        }
-        else if (moveAmountY < startPositionY - spriteHeight + 3)
-        {
-            transform.Translate(new Vector3(0, -spriteHeight, 0));
-            startPositionY -= spriteHeight;
+            if (Camera.main.transform.position.x > transform.position.x)
+            {
+                newSpritePosition.x += spriteWidth;
+            }
+            else if (Camera.main.transform.position.x < transform.position.x)
+            {
+                newSpritePosition.x -= spriteWidth;
+            }
+
+
+            if (Camera.main.transform.position.y > transform.position.y)
+            {
+                newSpritePosition.y += spriteHeight;
+            }
+            else if (Camera.main.transform.position.y < transform.position.y)
+            {
+                newSpritePosition.y -= spriteHeight;
+            }
+
+
+            transform.position = newSpritePosition;
         }
     }
 }
