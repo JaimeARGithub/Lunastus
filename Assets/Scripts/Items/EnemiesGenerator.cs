6e5e762dcc53cb43969825dcced367db699c1f66
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-07-28 23:25:52 +0200

    28/07/2024 23:25
    Añadida escena Corporation11. Pendiente meter spawn al instante + lógica para controlar el momento en que el jugador entra en la sala y que vaya contando 17.5 segundos.

--- Assets/Scripts/Items/EnemiesGenerator.cs
+++ Assets/Scripts/Items/EnemiesGenerator.cs
@@ -7,10 +7,11 @@ public class EnemiesGenerator : MonoBehaviour
     // Variables para las posiciones de generación de los bots marrones y amarillos
     Vector3[] brownRobotsGenerationPositions;
     Vector3[] yellowRobotsGenerationPositions;
+    Vector3[] redRobotsGenerationPositions;
 
     // Variables para la frecuencia de generación
     private float generationInstant = 0f;
-    private float generationCooldown = 10f;
+    private float generationCooldown = 17.5f;
 
     // Variables para los robots a generar
     public GameObject brownRobot;
@@ -22,23 +23,27 @@ public class EnemiesGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Asignnación del array de posiciones utilizado para el spawn de los BrownRobot
-        brownRobotsGenerationPositions = new Vector3[4];
-        brownRobotsGenerationPositions[0] = transform.position + new Vector3(-0.75f, 0, 0);
-        brownRobotsGenerationPositions[1] = transform.position + new Vector3(-0.25f, 0, 0);
-        brownRobotsGenerationPositions[2] = transform.position + new Vector3(0.25f, 0, 0);
-        brownRobotsGenerationPositions[3] = transform.position + new Vector3(0.75f, 0, 0);
+        // Asignnación del array de posiciones utilizado para el spawn de los BrownRobot + GreyRobot
+        brownRobotsGenerationPositions = new Vector3[5];
+        brownRobotsGenerationPositions[0] = transform.position + new Vector3(-1.25f, 0, 0);
+        brownRobotsGenerationPositions[1] = transform.position + new Vector3(-0.75f, 0, 0);
+        brownRobotsGenerationPositions[2] = transform.position + new Vector3(-0.25f, 0, 0);
+        brownRobotsGenerationPositions[3] = transform.position + new Vector3(0.25f, 0, 0);
+        brownRobotsGenerationPositions[4] = transform.position + new Vector3(0.75f, 0, 0);
 
 
-        // Asignación del array de posiciones utilizado para el spawn de los YellowRobot
-        yellowRobotsGenerationPositions = new Vector3[3];
-        yellowRobotsGenerationPositions[0] = transform.position + new Vector3(-0.5f, 0, 0);
-        yellowRobotsGenerationPositions[1] = transform.position;
-        yellowRobotsGenerationPositions[2] = transform.position + new Vector3(0.5f, 0, 0);
+        // Asignación del array de posiciones utilizado para el spawn de los YellowRobot + GreyRobot
+        yellowRobotsGenerationPositions = new Vector3[4];
+        yellowRobotsGenerationPositions[0] = transform.position + new Vector3(-1f, 0, 0);
+        yellowRobotsGenerationPositions[1] = transform.position + new Vector3(-0.5f, 0, 0);
+        yellowRobotsGenerationPositions[2] = transform.position;
+        yellowRobotsGenerationPositions[3] = transform.position + new Vector3(0.5f, 0, 0);
 
 
-        // Generación de enemigos inmediata
-        GenerateEnemies();
+        // Asignación del array de posiciones utilizado para el spawn de los RedRobot
+        redRobotsGenerationPositions = new Vector3[2];
+        redRobotsGenerationPositions[0] = transform.position + new Vector3(-0.35f, 0, 0);
+        redRobotsGenerationPositions[1] = transform.position + new Vector3(0.35f, 0, 0);
     }
 
     // Update is called once per frame
@@ -54,26 +59,42 @@ public class EnemiesGenerator : MonoBehaviour
 
     private void GenerateEnemies()
     {
-        int random = Random.Range(1, 101); //1 incluido, 101 excluido
+        int random = Random.Range(1, 4); //1 incluido, 4 excluido
 
-        if (random <= 35)
+
+        switch (random)
         {
-            for (int i = 0; i < brownRobotsGenerationPositions.Length; i++)
-            {
-                Instantiate(brownRobot, brownRobotsGenerationPositions[i], Quaternion.identity);
-            }
-        } else if (random > 35 && random <= 65)
-        {
-            for (int i = 0; i < yellowRobotsGenerationPositions.Length; i++)
-            {
-                Instantiate(yellowRobot, yellowRobotsGenerationPositions[i], Quaternion.identity);
-            }
-        } else if (random > 65 && random <= 85)
-        {
-            Instantiate(redRobot, transform.position, Quaternion.identity);
-        } else
-        {
-            Instantiate(greyRobot, transform.position, Quaternion.identity);
+            case 1:
+                for (int i = 0; i < brownRobotsGenerationPositions.Length; i++)
+                {
+                    if (i==0)
+                    {
+                        Instantiate(greyRobot, brownRobotsGenerationPositions[i], Quaternion.identity);
+
+                    } else
+                    {
+                        Instantiate(brownRobot, brownRobotsGenerationPositions[i], Quaternion.identity);
+                    }
+                }
+                break;
+            case 2:
+                for (int i = 0; i < yellowRobotsGenerationPositions.Length; i++)
+                {
+                    if (i==0)
+                    {
+                        Instantiate(greyRobot, brownRobotsGenerationPositions[i], Quaternion.identity);
+                    } else
+                    {
+                        Instantiate(yellowRobot, yellowRobotsGenerationPositions[i], Quaternion.identity);
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < redRobotsGenerationPositions.Length; i++)
+                {
+                    Instantiate(redRobot, redRobotsGenerationPositions[i], Quaternion.identity);
+                }
+                break;
         }
     }
 }
