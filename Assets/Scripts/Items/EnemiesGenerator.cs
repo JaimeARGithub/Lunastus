fdc53c95651d8db3f599f49ff40d46a620cb88ea
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-07-29 00:37:35 +0200

    29/07/2024 00:38
    Arreglada la lógica de generación de enemigos desde el EnemiesGenerator.

--- Assets/Scripts/Items/EnemiesGenerator.cs
+++ Assets/Scripts/Items/EnemiesGenerator.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class EnemiesGenerator : MonoBehaviour
 {
     // Variables para las posiciones de generación de los bots marrones y amarillos
-    Vector3[] brownRobotsGenerationPositions;
-    Vector3[] yellowRobotsGenerationPositions;
-    Vector3[] redRobotsGenerationPositions;
+    Vector3[] brownRobotsGenerationPositions = new Vector3[5];
+    Vector3[] yellowRobotsGenerationPositions = new Vector3[4];
+    Vector3[] redRobotsGenerationPositions = new Vector3[2];
 
     // Variables para la frecuencia de generación
-    private float generationInstant = 0f;
+    private float generationInstant;
     private float generationCooldown = 17.5f;
 
     // Variables para los robots a generar
@@ -23,27 +23,15 @@ public class EnemiesGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Asignnación del array de posiciones utilizado para el spawn de los BrownRobot + GreyRobot
-        brownRobotsGenerationPositions = new Vector3[5];
-        brownRobotsGenerationPositions[0] = transform.position + new Vector3(-1.25f, 0, 0);
-        brownRobotsGenerationPositions[1] = transform.position + new Vector3(-0.75f, 0, 0);
-        brownRobotsGenerationPositions[2] = transform.position + new Vector3(-0.25f, 0, 0);
-        brownRobotsGenerationPositions[3] = transform.position + new Vector3(0.25f, 0, 0);
-        brownRobotsGenerationPositions[4] = transform.position + new Vector3(0.75f, 0, 0);
+        InitializeGenerationPositions();
 
-
-        // Asignación del array de posiciones utilizado para el spawn de los YellowRobot + GreyRobot
-        yellowRobotsGenerationPositions = new Vector3[4];
-        yellowRobotsGenerationPositions[0] = transform.position + new Vector3(-1f, 0, 0);
-        yellowRobotsGenerationPositions[1] = transform.position + new Vector3(-0.5f, 0, 0);
-        yellowRobotsGenerationPositions[2] = transform.position;
-        yellowRobotsGenerationPositions[3] = transform.position + new Vector3(0.5f, 0, 0);
-
-
-        // Asignación del array de posiciones utilizado para el spawn de los RedRobot
-        redRobotsGenerationPositions = new Vector3[2];
-        redRobotsGenerationPositions[0] = transform.position + new Vector3(-0.35f, 0, 0);
-        redRobotsGenerationPositions[1] = transform.position + new Vector3(0.35f, 0, 0);
+        // En el instante de inicio, se ajusta la variable que marca el ritmo de generación a Time.time
+        // Ésto es para que, a la hora de generarse enemigos, no se tome como punto de referencia el tiempo desde que se inició
+        // el juego, sino el tiempo desde que se accede a la sala
+        // P.Ej.: Si generationInstant se deja a 0f y se llega a la sala habiendo transcurrido más de 17.5 segundos, los enemigos
+        // se generan al instante de entrar aunque no haya un GenerateEnemies() en el método Start()
+        generationInstant = Time.time;
+        GenerateEnemies();
     }
 
     // Update is called once per frame
@@ -82,7 +70,7 @@ public class EnemiesGenerator : MonoBehaviour
                 {
                     if (i==0)
                     {
-                        Instantiate(greyRobot, brownRobotsGenerationPositions[i], Quaternion.identity);
+                        Instantiate(greyRobot, yellowRobotsGenerationPositions[i], Quaternion.identity);
                     } else
                     {
                         Instantiate(yellowRobot, yellowRobotsGenerationPositions[i], Quaternion.identity);
@@ -96,5 +84,27 @@ public class EnemiesGenerator : MonoBehaviour
                 }
                 break;
         }
+    }
+
+    private void InitializeGenerationPositions()
+    {
+        // Asignnación del array de posiciones utilizado para el spawn de los BrownRobot + GreyRobot
+        brownRobotsGenerationPositions[0] = transform.position + new Vector3(-1.25f, 0, 0);
+        brownRobotsGenerationPositions[1] = transform.position + new Vector3(-0.75f, 0, 0);
+        brownRobotsGenerationPositions[2] = transform.position + new Vector3(-0.25f, 0, 0);
+        brownRobotsGenerationPositions[3] = transform.position + new Vector3(0.25f, 0, 0);
+        brownRobotsGenerationPositions[4] = transform.position + new Vector3(0.75f, 0, 0);
+
+
+        // Asignación del array de posiciones utilizado para el spawn de los YellowRobot + GreyRobot
+        yellowRobotsGenerationPositions[0] = transform.position + new Vector3(-1f, 0, 0);
+        yellowRobotsGenerationPositions[1] = transform.position + new Vector3(-0.5f, 0, 0);
+        yellowRobotsGenerationPositions[2] = transform.position;
+        yellowRobotsGenerationPositions[3] = transform.position + new Vector3(0.5f, 0, 0);
+
+
+        // Asignación del array de posiciones utilizado para el spawn de los RedRobot
+        redRobotsGenerationPositions[0] = transform.position + new Vector3(-0.35f, 0, 0);
+        redRobotsGenerationPositions[1] = transform.position + new Vector3(0.35f, 0, 0);
     }
 }
