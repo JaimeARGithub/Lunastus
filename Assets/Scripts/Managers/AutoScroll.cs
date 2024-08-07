6311370e3c85158699db45b24ba8c696ca5f10bb
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2024-08-07 03:06:01 +0200

    07/08/2024 03:06
    Actualizados los créditos; salvo el equipo de beta-testers, todo incluido.

--- Assets/Scripts/Managers/AutoScroll.cs
+++ Assets/Scripts/Managers/AutoScroll.cs
@@ -6,9 +6,9 @@ using TMPro;
 public class AutoScroll : MonoBehaviour
 {
     private float speed = 20f;
-    private float textPositionBegin = -2600f;
-    private float textPositionEnd = 2600f;
-    private float textPositionEndResetLimit = 2595f;
+    private float textPositionBegin = -2800f;
+    private float textPositionEnd = 2800f;
+    private float textPositionEndResetLimit = 2795f;
 
     RectTransform gameObjectRectTransform;
     [SerializeField] public TextMeshProUGUI creditsText;
